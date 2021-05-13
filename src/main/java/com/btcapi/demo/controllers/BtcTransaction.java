a3ab402634c936f3dd0fb98d758426d7cd84a105
leaderruvuna <ruvunaraymond@gmail.com>
2021-05-13 17:11:43 +0300

    Feat: Add endpoints features with services

--- src/main/java/com/btcapi/demo/controllers/BtcTransaction.java
+++ src/main/java/com/btcapi/demo/controllers/BtcTransaction.java
@@ -1,41 +1,93 @@
 package com.btcapi.demo.controllers;
 
+import com.btcapi.demo.modals.TransactionsModal;
+import com.btcapi.demo.services.BtcTransactionService;
+import com.btcapi.demo.utils.validators.BtcTransactionInputsValidator;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/gateway/transaction")
 public class BtcTransaction {
 
+    private BtcTransactionService btcTransactionService;
+    private BtcTransactionInputsValidator btcTransactionInputsValidator;
+
+    public BtcTransaction(BtcTransactionService btcTransactionService, BtcTransactionInputsValidator btcTransactionInputsValidator) {
+        this.btcTransactionService = btcTransactionService;
+        this.btcTransactionInputsValidator = btcTransactionInputsValidator;
+    }
+
+    @Async
     @PostMapping("/btc/create")
-    ResponseEntity createTransaction() {
-        return null;
+    ResponseEntity createTransaction(@RequestBody TransactionsModal transactionsModal) throws IOException {
+        String inputAddress = transactionsModal.getInputAddress();
+        String outputAddress = transactionsModal.getOutputAddress();
+        double amount = transactionsModal.getAmount();
+        if (btcTransactionInputsValidator.isTransInputsCreation(inputAddress, outputAddress, amount)) {
+            ResponseEntity transData = (ResponseEntity) btcTransactionService.txCreation(inputAddress, outputAddress, amount);
+            return new ResponseEntity(transData, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity("All fields are required!", HttpStatus.FORBIDDEN);
+        }
     }
 
+    @Async
     @PostMapping("/btc/sign")
-    ResponseEntity signTransaction() {
-        return null;
+    ResponseEntity signTransaction(String transaction, String toSign, String signatures, String pubKeys) throws IOException {
+        if (btcTransactionInputsValidator.isTransInputsSign(transaction, toSign, signatures, pubKeys)) {
+            ResponseEntity transSignData = (ResponseEntity) btcTransactionService.txSignHx(transaction, toSign, signatures, pubKeys);
+            return new ResponseEntity(transSignData, HttpStatus.OK);
+        } else {
+            return new ResponseEntity("All fields are required!", HttpStatus.FORBIDDEN);
+        }
     }
 
+    @Async
     @PostMapping("/btc/push")
-    ResponseEntity pushTransaction() {
-        return null;
+    ResponseEntity pushTransaction(String transaction) throws IOException {
+        if (btcTransactionInputsValidator.isTransInputsPush(transaction)) {
+            ResponseEntity transPushData = (ResponseEntity) btcTransactionService.txPush(transaction);
+            return new ResponseEntity(transPushData, HttpStatus.OK);
+        } else {
+            return new ResponseEntity("All fields are required!", HttpStatus.FORBIDDEN);
+        }
     }
 
+    @Async
     @PostMapping("/btc/decode")
-    ResponseEntity decodeTransaction() {
-        return null;
+    ResponseEntity decodeTransaction(String transaction) throws IOException {
+        if (btcTransactionInputsValidator.isTransInputsDecode(transaction)) {
+            ResponseEntity transDecodeData = (ResponseEntity) btcTransactionService.txDecode(transaction);
+            return new ResponseEntity(transDecodeData, HttpStatus.OK);
+        } else {
+            return new ResponseEntity("All fields are required!", HttpStatus.FORBIDDEN);
+        }
     }
 
+    @Async
     @PostMapping("/btc/witness")
-    ResponseEntity witnessTransaction() {
-        return null;
+    ResponseEntity witnessTransaction(String transaction) throws IOException {
+        if (btcTransactionInputsValidator.isTransInputsWitness(transaction)) {
+            ResponseEntity transWitnessData = (ResponseEntity) btcTransactionService.txWitness(transaction);
+            return new ResponseEntity(transWitnessData, HttpStatus.OK);
+        } else {
+            return new ResponseEntity("All fields are required!", HttpStatus.FORBIDDEN);
+        }
     }
 
+    @Async
     @PostMapping("/btc/propagate")
-    ResponseEntity propagateTransaction() {
-        return null;
+    ResponseEntity propagateTransaction(String transaction) throws IOException {
+        if (btcTransactionInputsValidator.isTransInputsPropagate(transaction)) {
+            ResponseEntity transWitnessData = (ResponseEntity) btcTransactionService.txPropagation(transaction);
+            return new ResponseEntity(transWitnessData, HttpStatus.OK);
+        } else {
+            return new ResponseEntity("All fields are required!", HttpStatus.FORBIDDEN);
+        }
     }
 }
--- src/main/java/com/btcapi/demo/utils/validators/BtcTransactionInputsValidator.java
+++ src/main/java/com/btcapi/demo/utils/validators/BtcTransactionInputsValidator.java
@@ -1,15 +1,52 @@
 package com.btcapi.demo.utils.validators;
 
+import com.btcapi.demo.modals.TransactionsModal;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BtcTransactionInputsValidator {
 
-    public boolean isTransInputsCreation() {
-        return false;
+    public boolean isTransInputsCreation(String fromAddress,String toAddress, double amount) {
+
+        if (fromAddress.isEmpty() || toAddress.isEmpty() || amount == 0) {
+            return false;
+        }
+        return true;
+
     }
 
-    public boolean isTransInputsSign() {
-        return false;
+    public boolean isTransInputsSign(String transaction, String toSign, String signatures, String pubKeys) {
+        if (transaction.isEmpty() || toSign.isEmpty() || signatures.isEmpty() || pubKeys.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isTransInputsDecode(String transaction) {
+        if (transaction.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isTransInputsPush(String transaction) {
+        if (transaction.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isTransInputsWitness(String transaction) {
+        if (transaction.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isTransInputsPropagate(String transaction) {
+        if (transaction.isEmpty()) {
+            return false;
+        }
+        return true;
     }
 }
